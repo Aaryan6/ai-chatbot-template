c00bbb6cf983fed1817edf7994dd0924dfa548b1
Aaryan6 <aaryanpatel683@gmail.com>
2023-11-23 18:25:11 +0530

    first commit

--- components/suggested-question-form.tsx
+++ components/suggested-question-form.tsx
@@ -14,7 +14,7 @@ export default function SuggestedQuestionForm({
 }: ChatMessageProps) {
   return (
     <form
-      className="rounded-md bg-white px-4 py-2 text-gray-600 hover:text-gray-800 cursor-pointer"
+      className="cursor-pointer rounded-md bg-white px-4 py-2 text-gray-600 hover:text-gray-800"
       onClick={() => setInput(ques.question)}
       onSubmit={async e => {
         e.preventDefault()
